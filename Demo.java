7349abfb2e2aca50a03c34cfb58a7ad9bf25e398
rishabh <rishabhmarathe09@gmail.com>
2025-03-01 18:37:59 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,15 @@
+package Java_Topics;
+
+
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I am");
+		
+		System.out.println("Rushabh");	
+		
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,22 @@
+package Class_And_Object;
+
+public class Student {
+	
+	
+	int no=12;
+	String name="Rushabh";
+	
+	public static void main(String[] args) {
+		
+	Student S=new Student();
+	
+
+	
+	System.out.println(S.no);
+	System.out.println(S.name);
+		
+		
+		
+	}
+
+}
